da2f90280dc4f2a5d5f91185af77e353333e5411
Daniel Vinicius <daniel.vinicius.sviana@gmail.com>
2026-09-04 11:24:34 -0300

    Segundo teste

--- tests.py
+++ tests.py
@@ -77,3 +77,27 @@ def test_lista_imoveis_com_registros(client, mock_db):
     mock_cursor.execute.assert_called_once_with("SELECT * FROM imoveis")
     mock_cursor.fetchall.assert_called_once()
     mock_conn.close.assert_called_once()
+
+def test_find_imovel_by_id(client, mock_db):
+    _, mock_conn, mock_cursor = mock_db
+    mock_cursor.fetchone.return_value = {
+        "id": 2,
+        "logradouro": "Price Prairie",
+        "tipo_logradouro": "Travessa",
+        "bairro": "Colonton",
+        "cidade": "North Garyville",
+        "cep": "93354",
+        "tipo": "casa em condominio",
+        "valor": 260069.89,
+        "data_aquisicao": "2021-11-30",
+    }
+
+    response = client.get("/imoveis/2")
+
+    assert response.status_code == 200
+    assert response.get_json() == mock_cursor.fetchone.return_value
+
+    mock_cursor.execute.assert_called_once_with("SELECT * FROM imoveis where id = ?", (2,))
+    mock_cursor.fetchone.assert_called_once()
+    mock_conn.close.assert_called_once()
+
